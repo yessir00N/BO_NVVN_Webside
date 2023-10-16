6f07601812cda9aa33f445d2158708c755f62abf
Yes sin <34749@ma-web.nl>
2023-10-16 14:11:44 +0200

    docker setup af

---
+++ source/data.php
@@ -0,0 +1,75 @@
+<?php
+
+$library = [
+    $item_1 = [
+        'artist' => 'Travis Scott', 
+        'title' => 'goosebumps ft. Kendrick Lamar', 
+        'genre' => 'trap-hop', 
+        'year' => 2017,
+        'time' => '4:10',
+        'img' => 'img/travis_scott.jpg',
+    ],
+    $item_2 = [
+        'artist' => 'POP SMOKE', 
+        'title' => 'FLEXING', 
+        'genre' => 'Hip Hop', 'Rap', 
+        'year' => 2019,
+        'time' => '2:17'
+    ],
+    $item_3 = [
+        'artist' => 'Chief Keef', 
+        'title' => 'I Dont Like', 
+        'genre' => 'Hip Hop', 'Rap', 
+        'year' => 2015,
+        'time' => '4:54'
+    ],
+    $item_4 = [
+        'artist' => 'Future, Juice WRLD', 
+        'title' => 'Fine china', 
+        'genre' => 'Hip hop', 
+        'year' => 2018,
+        'time' => '2:23'
+    ],
+    $item_2 = [
+        'artist' => 'POP SMOKE', 
+        'title' => 'FLEXING', 
+        'genre' => 'Hip Hop', 'Rap', 
+        'year' => 2019,
+        'time' => '2:17'
+    ],
+    $item_3 = [
+        'artist' => 'Chief Keef', 
+        'title' => 'I Dont Like', 
+        'genre' => 'Hip Hop', 'Rap', 
+        'year' => 2015,
+        'time' => '4:54'
+    ],
+    $item_4 = [
+        'artist' => 'Future, Juice WRLD', 
+        'title' => 'Fine china', 
+        'genre' => 'Hip hop', 
+        'year' => 2018,
+        'time' => '2:23'
+    ],
+    $item_2 = [
+        'artist' => 'POP SMOKE', 
+        'title' => 'FLEXING', 
+        'genre' => 'Hip Hop', 'Rap', 
+        'year' => 2019,
+        'time' => '2:17'
+    ],
+    $item_3 = [
+        'artist' => 'Chief Keef', 
+        'title' => 'I Dont Like', 
+        'genre' => 'Hip Hop', 'Rap', 
+        'year' => 2015,
+        'time' => '4:54'
+    ],
+    $item_4 = [
+        'artist' => 'Future, Juice WRLD', 
+        'title' => 'Fine china', 
+        'genre' => 'Hip hop', 
+        'year' => 2018,
+        'time' => '2:23'
+    ],
+];
